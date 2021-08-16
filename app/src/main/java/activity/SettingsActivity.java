f1ca9e1ad7364e2874388e266aa1a205fa8b8a8b
Jonas Elmesten <jonas_elmesten@hotmail.com>
2021-08-16 12:18:55 +0200

    Updated settings gui and added base logic for remove reviews or deck data.

--- app/src/main/java/activity/SettingsActivity.java
+++ app/src/main/java/activity/SettingsActivity.java
@@ -1,8 +1,12 @@
 package activity;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.Button;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 import com.example.learndeck.R;
+
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -11,5 +15,55 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        Button removeDeckButton = findViewById(R.id.removeDeckButton);
+        removeDeckButton.setOnClickListener(view -> {
+            removeDeck();
+        });
+
+        Button resetReviewsButton = findViewById(R.id.resetViewsButton);
+        resetReviewsButton.setOnClickListener(view -> {
+            resetReviews();
+        });
+    }
+
+    private void resetReviews() {
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle("All your study data will be removed. Are you sure?");
+
+        alertDialogBuilder
+                .setCancelable(false)
+                .setPositiveButton("Yes",
+                        (dialog, id) -> {
+                            finish();
+                        })
+
+                .setNegativeButton("No", (dialog, id) -> dialog.cancel());
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+    private void removeDeck() {
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle("You'll no longer be able to study this deck. Are you sure?");
+
+        alertDialogBuilder
+                .setCancelable(false)
+                .setPositiveButton("Yes",
+                        (dialog, id) -> {
+                            finish();
+                        })
+
+                .setNegativeButton("No", (dialog, id) -> dialog.cancel());
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
     }
 }
